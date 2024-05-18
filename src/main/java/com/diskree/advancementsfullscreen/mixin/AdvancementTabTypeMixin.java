7de8535c1fb133bdcbc49d1b0bdb72955c0718a2
diskree <diskreee@gmail.com>
2024-05-18 03:27:43 +0300

    Draw middle tab texture instead last

--- src/main/java/com/diskree/advancementsfullscreen/mixin/AdvancementTabTypeMixin.java
+++ src/main/java/com/diskree/advancementsfullscreen/mixin/AdvancementTabTypeMixin.java
@@ -3,9 +3,13 @@ package com.diskree.advancementsfullscreen.mixin;
 import com.diskree.advancementsfullscreen.injection.AdvancementsScreenImpl;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gui.screen.advancement.AdvancementTabType;
+import net.minecraft.util.Identifier;
+import org.jetbrains.annotations.NotNull;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Constant;
 import org.spongepowered.asm.mixin.injection.ModifyConstant;
+import org.spongepowered.asm.mixin.injection.Redirect;
 
 import static net.minecraft.client.gui.screen.advancement.AdvancementsScreen.WINDOW_HEIGHT;
 import static net.minecraft.client.gui.screen.advancement.AdvancementsScreen.WINDOW_WIDTH;
@@ -39,5 +43,16 @@ public class AdvancementTabTypeMixin {
             return screenImpl.advancementsfullscreen$getFullscreenWindowHeight(true) - 4;
         }
         return originalValue;
+    }
+
+    @Redirect(
+        method = "drawBackground",
+        at = @At(
+            value = "INVOKE",
+            target = "Lnet/minecraft/client/gui/screen/advancement/AdvancementTabType$Textures;last()Lnet/minecraft/util/Identifier;"
+        )
+    )
+    public Identifier drawMiddleBackgroundInsteadLast(@NotNull AdvancementTabType.Textures textures) {
+        return textures.middle();
     }
 }
